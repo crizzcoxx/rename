7f975bce2929c11265a1e1eac69a1ed52de6fe91
crizzcoxx <crizzcoxx@gmail.com>
2016-09-23 21:35:38 -0700

    more changes

--- src/header1.js
+++ src/header1.js
@@ -1,10 +1,15 @@
 var React = require('react')
 var ReactDom = require('react-dom')
-var h1 = React.Dom.h1
+var h1 = React.DOM.h1
 
-var header1 = React.render () ({
-	return {
-		div(createElement())
+var header1 = React.createClass ({
+	render () {
+		return(
+			div(null, 
+				h1({style: {color: this.props.color}}, this.props.title)
+			)
+		)
 	}
 })
 
+module.exports = header1
