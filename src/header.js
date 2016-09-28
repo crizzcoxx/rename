7fab37a217310563477dbe799f9e837d332ab2d2
crizzcoxx <crizzcoxx@gmail.com>
2016-09-27 19:40:05 -0700

    Adding Header

---
+++ src/header.js
@@ -0,0 +1,14 @@
+React = require('react')
+ReactDOM = require('react-dom')
+
+var head = React.createClass ({
+	render () {
+		return(
+			div(null,
+				header({style {background-color: }})
+				)
+		)
+	}
+})
+
+module.exports('header')
